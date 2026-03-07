5a00428d89184b94f6df2905d670ad97390c925b
Justin Keys <sir.different@gmail.com>
2026-03-06 22:42:09 -0800

    Adding routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from '../home/home';
+import { Designer } from '../designer/designer';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home
+    },
+    {
+        path: 'designer',
+        component: Designer
+    }
+];
