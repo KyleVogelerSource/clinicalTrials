1b74277be9ae6c29cca47bcb8a8512daae206f9d
Justin Keys <sir.different@gmail.com>
2026-03-07 23:13:50 -0800

    Adding mock suggestions for disease entry

--- frontend/src/pages/designer/designer.ts
+++ frontend/src/pages/designer/designer.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
-import { ReactiveFormsModule, FormGroup, FormControl } from "@angular/forms";
+import { ChangeDetectionStrategy, Component, signal } from "@angular/core";
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from "@angular/forms";
 import { ProgressTrack } from "../../primitives/progress-track/progress-track";
+import { debounceTime, distinctUntilChanged, map } from "rxjs";
 
 export enum PhaseEnum {
     PHASE_1 = 'Phase I',
@@ -29,6 +30,8 @@ export enum AllocationEnum {
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Designer {
+    conditionMatches = signal<string[]>([]);
+
     phaseOptions = Object.values(PhaseEnum);
     sexOptions = Object.values(SexEnum);
     allocationOptions = Object.values(AllocationEnum);
@@ -39,10 +42,18 @@ export class Designer {
         allocationType: new FormControl<AllocationEnum>(AllocationEnum.NA),
         interventionModel: new FormControl(''),
         blindingType: new FormControl(''),
-        minAge: new FormControl<number | null>(null),
-        maxAge: new FormControl<number | null>(null),
+        minAge: new FormControl<number | null>(null, [Validators.min(0), Validators.max(150)]),
+        maxAge: new FormControl<number | null>(null, [Validators.min(0), Validators.max(150)]),
         sex: new FormControl<SexEnum>(SexEnum.Any)
     });
+
+    constructor() {
+        this.inputForm.controls.condition.valueChanges.pipe(
+            debounceTime(300),
+            distinctUntilChanged(),
+            map(val => [val + 'test', val + 'one', val + 'two', val + 'three'])
+        ).subscribe(matches => this.conditionMatches.set(matches));
+    }
 
     onNext() {
         console.log('Form data:', this.inputForm.value);
