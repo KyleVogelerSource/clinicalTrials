ab27cd6a72dc656e3b3e2ea47b78827e46d9d54e
KyleVogelerSource <vogeler.kyle@gmail.com>
2026-04-12 15:20:12 -0400

    undo test change

--- backend/vitest.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vitest/config";
-
-export default defineConfig({
-  test: {
-    include: ["src/**/*.spec.ts", "src/**/*.test.ts"],
-    exclude: ["src/**/*.integration.spec.ts", "**/node_modules/**"],
-  },
-});
