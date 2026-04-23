cbe354e82f490c41f9526be0770e55a14d0ac39f
KyleVogelerSource <vogeler.kyle@gmail.com>
2026-04-23 19:57:44 -0400

    Fix more tests

--- frontend/src/services/trial-workflow-service.ts
+++ frontend/src/services/trial-workflow-service.ts
@@ -366,6 +366,8 @@ export class TrialWorkflowService {
             requiredConditions: input.required ?? [],
             ineligibleConditions: input.ineligible ?? [],
             selectedTrialIds: this.selectedTrialIds(),
+            inclusionCriteria: input.inclusionCriteria ?? [],
+            exclusionCriteria: input.exclusionCriteria ?? [],
         };
 
         return request;
