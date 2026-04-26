6499d99cdfbc64d42b8d64a58a6c8841957ecfff
Justin Keys <sir.different@gmail.com>
2026-04-26 03:38:02 -0700

    Using custom dropdowns on analysis page

--- frontend/src/primitives/custom-select/custom-select.ts
+++ frontend/src/primitives/custom-select/custom-select.ts
@@ -23,6 +23,7 @@ export class CustomSelect implements ControlValueAccessor {
     options = input.required<string[]>();
     descriptions = input<Record<string, string>>({});
     placeholder = input<string>('Select an option...');
+    width = input<string>('100%');
 
     elementRef = inject(ElementRef);
     
@@ -31,6 +32,8 @@ export class CustomSelect implements ControlValueAccessor {
     hoveredOption = signal<string | null>(null);
     tooltipTop = signal<number>(0);
     tooltipLeft = signal<number>(0);
+    tooltipPosition = signal<'left' | 'right'>('right');
+    dropdownAlignment = signal<'left' | 'right'>('left');
 
     // ControlValueAccessor methods
     onChange: any = () => {};
@@ -53,7 +56,21 @@ export class CustomSelect implements ControlValueAccessor {
     }
 
     toggleDropdown() {
+        if (!this.isOpen()) {
+            this.calculateDropdownAlignment();
+        }
         this.isOpen.update(v => !v);
+    }
+
+    private calculateDropdownAlignment() {
+        const rect = this.elementRef.nativeElement.getBoundingClientRect();
+        const screenWidth = window.innerWidth;
+        // If less than 300px available to the right of component start, align dropdown to right (expand left)
+        if (screenWidth - rect.left < 300) {
+            this.dropdownAlignment.set('right');
+        } else {
+            this.dropdownAlignment.set('left');
+        }
     }
 
     selectOption(option: string) {
@@ -88,7 +105,18 @@ export class CustomSelect implements ControlValueAccessor {
 
     private updateTooltipPosition(element: HTMLElement) {
         const rect = element.getBoundingClientRect();
-        this.tooltipLeft.set(rect.right + 10);
+        const screenWidth = window.innerWidth;
+        const tooltipWidth = 240; // max-width + padding allowance
+
+        if (rect.right + tooltipWidth > screenWidth) {
+            // Not enough room on right, flip to left of the item
+            this.tooltipPosition.set('left');
+            this.tooltipLeft.set(rect.left - 10);
+        } else {
+            this.tooltipPosition.set('right');
+            this.tooltipLeft.set(rect.right + 10);
+        }
+        
         this.tooltipTop.set(rect.top + (rect.height / 2));
     }
 }
